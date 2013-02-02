9620bd31d8e3ad2f1ae3f443eb8525ab0b2c4b94
parkhomenko <stanislav_parkhomenko@yahoo.com>
2013-02-02 23:27:00 +0200

    tree

---
+++ mvvm/Tree/IItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvm.Tree
+{
+    interface IItem
+    {
+        void AddItem(IItem item) { throw new Exception("Not supported"); }
+        void RemoveItem(IItem item) { throw new Exception("Not supported"); }
+
+        string SomeName { get; set; }
+        string SomeDescription { get; set; }
+    }
+}
---
+++ mvvm/Tree/TreeViewItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvm.Tree
+{
+    class TreeViewItem : IItem
+    {
+        List<IItem> items = new List<IItem>();
+
+        public void AddItem(IItem item)
+        {
+            items.Add(item);
+        }
+
+        public void RemoveItem(IItem item)
+        {
+            items.Remove(item);
+        }
+    }
+}
